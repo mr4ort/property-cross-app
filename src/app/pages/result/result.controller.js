0b46aa157e818fb1fdb7b249b6994b714fef4104
Igor Datsenko <mr4ort@gmail.com>
2016-11-06 22:16:53 +0200

    update

--- src/app/pages/result/result.controller.js
+++ src/app/pages/result/result.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function ResultController($log, dataStorage) {
+function ResultController(dataStorage, getData) {
   'ngInject';
 
   const cnt = this;
@@ -11,18 +11,60 @@ function ResultController($log, dataStorage) {
 
   cnt.dataForDispaing = {};
 
+  function downloadData(dataReqest) {
+    getData(dataReqest).then(function successCallback(response) {
+      fillData(response.data.response);
+
+    }, function errorCallback(response) {
+      console.log(response.data.response.application_response_code + response.data.response.application_response_text);
+    });
+  }
+
+  function fillData(data) {
+
+    if (cnt.dataForDispaing.place_name === undefined) {
+      cnt.dataForDispaing.place_name = data.locations[0].place_name;
+      cnt.dataForDispaing.searchResult = data.listings;
+      cnt.dataForDispaing.totalLenth = data.total_results;
+      cnt.dataForDispaing.totalPages = data.total_pages;
+      cnt.dataForDispaing.corentPages = data.page;
+    } else {
+      cnt.dataForDispaing.searchResult = cnt.dataForDispaing.searchResult.concat(data.listings);
+      cnt.dataForDispaing.corentPages = data.page;
+    }
+
+    console.log(cnt.dataForDispaing);
+
+  }
 
   if (searchResult) {
     dataStorage.set('search-result');
-    cnt.dataForDispaing.place_name = searchResult.locations[0].place_name;
-    cnt.dataForDispaing.searchResult = searchResult.listings;
-    cnt.dataForDispaing.totalLenth = searchResult.total_results;
-    cnt.dataForDispaing.totalPages = searchResult.total_pages;
-    console.log(cnt);
-  } else if (placeName) {
-    dataStorage.set('place-name');
-    console.log('downloading data');
+    fillData(searchResult);
   }
+  else if (placeName) {
+    dataStorage.set('place-name');
+
+    let requestData = {
+      country: 'ua',
+      place_name: placeName
+    };
+
+    downloadData(requestData);
+  }
+  
+  cnt.loadMore = function () {
+
+    if (cnt.dataForDispaing.corentPages < cnt.dataForDispaing.totalPages) {
+      let requestData = {
+        country: 'ua',
+        place_name: placeName,
+        page: cnt.dataForDispaing.corentPages + 1
+      };
+
+      downloadData(requestData);
+    }
+  }
+
 
 }
 
